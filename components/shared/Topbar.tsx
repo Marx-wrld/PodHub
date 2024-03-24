b79d8912b2f41243d681a81900b651d1be76d6df
Ian Marx <iongonga@kabarak.ac.ke>
2024-03-24 18:55:52 +0300

    topbar dark theme set

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
     return (
@@ -31,6 +32,7 @@ function Topbar() {
                 </div>
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger:
                             "py-2 px-4"
