e2b133f50e5c4a33e539bad8478956f3e626c77a
Ian Marx <iongonga@kabarak.ac.ke>
2024-06-04 14:41:45 +0300

    Project setup and routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,43 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import Topbar from "@/components/shared/Topbar";
-import Bottombar from "@/components/shared/Bottombar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Threads",
-  description: "A place to discuss ideas and share knowledge with others.",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <Topbar />
-
-          <main className="flex flex-row">
-            <LeftSidebar/>
-            <section className="main-container">
-              <div className="w-full max-w-full">
-                {children}
-              </div>
-            </section>
-            <RightSidebar/>
-          </main>
-
-          <Bottombar />
-        </body>
-      </html>
-    </ClerkProvider>
+    <div>
+        <main>
+            <p className="text-white-1">LEFT SIDEBAR</p>
+            {children} 
+            <p className="text-white-1">RIGHT SIDEBAR</p>
+        </main>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-      experimental: {
-             serverActions: true,
-             serverComponentsExternalPackages: ["mongoose"],
-        },
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "img.clerk.com",
-          },
-          {
-            protocol: "https",
-            hostname: "images.clerk.dev",
-          },
-          {
-            protocol: "https",
-            hostname: "uploadthing.com",
-          },
-          {
-            protocol: "https",
-            hostname: "placehold.co",
-          },
-        ],
-        // typescript: {
-        //   ignoreBuildErrors: true,
-        // },
-      },
-    };
+const nextConfig = {};
 
 export default nextConfig;
